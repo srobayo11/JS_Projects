caddea785ec2801d7aa52e0e51580da490bcadee
Sebastian Robayo <srobayo@gmail.com>
2020-11-26 14:53:38 +1000

    HTML and CSS of challenge 5 done, rearrangement of images

--- static/js/script.js
+++ static/js/script.js
@@ -144,3 +144,20 @@ function randomColors() {
         all_buttons[i].classList.add(randomChoice);
     }
 }
+
+//challenge 5: Blackjack
+
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+};
+
+const YOU = blackjackGame['You'];
+const DEALER = blackjackGame['dealer'];
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+function blackjackHit(){
+    let cardImage = document.createElement('img');
+    cardImage
+}
